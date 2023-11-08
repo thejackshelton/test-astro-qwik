8c71915bb0e6d34ed0a562dfc7fd781189d0647d
thejackshelton <me@jackshelton.com>
2023-11-08 16:47:19 -0600

    test static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "astro/config";
 import qwikdev from "@qwikdev/astro";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev()],
-  output: "server",
-  adapter: netlify(),
 });
