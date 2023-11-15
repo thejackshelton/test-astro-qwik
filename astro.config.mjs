8b42506450ee145d0c4b177ab5b9fd3a54a8e53b
thejackshelton <me@jackshelton.com>
2023-11-15 09:57:11 -0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
-import qwikdev from "@qwikdev/astro";
+// import qwikdev from "@qwikdev/astro";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev()],
+  integrations: [],
   output: "server",
   adapter: cloudflare(),
 });
